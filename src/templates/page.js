2717b650a04b07c2bfbcaf7a08d23aa4b41e0526
Daito <50580495+daito11@users.noreply.github.com>
2020-04-20 18:40:41 +0200

    Update page.js

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import _ from 'lodash';
 
-import {Layout} from '../components/index';
-import {safePrefix, htmlToReact} from '../utils';
+import components, {Layout} from '../components/index';
 
-export default class Page extends React.Component {
+export default class Home extends React.Component {
     render() {
         return (
             <Layout {...this.props}>
                 <div id="wrapper">
-                    <section id="main" className="wrapper">
-                        <div className="inner">
-                            <h1 className="major">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
-                            {_.get(this.props, 'pageContext.frontmatter.content_img_path') && 
-                                <span className="image fit"><img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.content_img_path'))} alt="" /></span>
-                            }
-                            {htmlToReact(_.get(this.props, 'pageContext.html'))}
-                        </div>
-                    </section>
+                    {_.map(_.get(this.props, 'pageContext.frontmatter.sections'), (section, section_idx) => {
+                        let GetSectionComponent = components[_.get(section, 'component')];
+                        return (
+                            <GetSectionComponent key={section_idx} {...this.props} section={section} site={this.props.pageContext.site} page={this.props.pageContext} />
+                        )
+                    })}
                 </div>
             </Layout>
         );
